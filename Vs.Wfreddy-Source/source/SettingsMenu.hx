dad5ce4c2f4d77cdcb0deced13a935b573b34187
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-20 11:34:14 -0300

    Update SettingsMenu.hx

--- Vs.Wfreddy-Source/source/SettingsMenu.hx
+++ Vs.Wfreddy-Source/source/SettingsMenu.hx
@@ -22,7 +22,9 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+#if newgrounds 
 import io.newgrounds.NG;
+#end
 import lime.app.Application;
 import openfl.Assets;
 
@@ -230,14 +232,14 @@ class SettingsMenu extends MusicBeatState
 
 		
 		
-		#if polymod
+		/*#if polymod
 		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
 		#end
 		
 		#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
+		#end*/
 
 		@:privateAccess
 		{
