56af62f80bb126fc57cc25195e1b3ae09f969bda
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-20 11:48:53 -0300

    Update Main.hx

--- Vs.Wfreddy-Source/source/Main.hx
+++ Vs.Wfreddy-Source/source/Main.hx
@@ -74,19 +74,19 @@ class Main extends Sprite
 		}
 
 		#if !debug
-		initialState = WarningState; //CHANGE THIS TO BIRDY MENU LOL
+	//	initialState = WarningState; //CHANGE THIS TO BIRDY MENU LOL
 		#end
 
 		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
 
 		addChild(game);
 
-		#if !mobile
+	//	#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
 
-		#end
+	//	#end
 	}
 
 	var game:FlxGame;
