65142e788ef6d4ffa3bde8f6eeb0d37c5cfc2a5f
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-05-20 11:50:33 -0300

    Update FreddyFreeplay.hx

--- Vs.Wfreddy-Source/source/FreddyFreeplay.hx
+++ Vs.Wfreddy-Source/source/FreddyFreeplay.hx
@@ -22,7 +22,9 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
 import flixel.util.FlxTimer;
+#if newgrounds 
 import io.newgrounds.NG;
+#end
 import lime.app.Application;
 import openfl.Assets;
 
@@ -249,14 +251,14 @@ class FreddyFreeplay extends MusicBeatState
 
 		
 		
-		#if polymod
+		/*#if polymod
 		polymod.Polymod.init({modRoot: "mods", dirs: ['introMod']});
 		#end
 		
 		#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
+		#end*/
 
 		@:privateAccess
 		{
